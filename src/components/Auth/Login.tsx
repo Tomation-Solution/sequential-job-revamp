0dede18f6c572b8c11d3173db0ea22924f74df4e
markothedev <markothedevmail@gmail.com>
2023-03-28 04:32:16 +0100

    installed react-hook-form yup and react query... also  created a reuseable input field

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -7,16 +7,32 @@ import {
 import {
   Form,
   FormContainer,
-  FormInput,
 } from "../../globals/styles/forms.styles";
 import { Link } from "react-router-dom";
 import EastIcon from "@mui/icons-material/East";
 import Logo from "../../assets/Logo.png";
-
+import InputWithLabel from "../InputWithLabel/InputWithLabel";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 // styles here were copied from signups.styles.tsx and forms.styles.tsx
 // and then modified to fit the needs of this component
 
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+  full_name:yup.string().required(),
+  password:yup.string().required(),
+  confirm_password:yup.string().oneOf([yup.ref('password')],'Passwords must match'),
+  phone:yup.number().min(11).required(),
+})
+
+type FormData = yup.InferType<typeof schema>;
 const Login = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  });
   return (
     <SignUpContainer>
       <SignUpWrapper>
@@ -29,19 +45,12 @@ const Login = () => {
 
           <DecisionContainer>
             <Form>
-              <FormInput>
-                <label htmlFor="phoneNumber">Phone Number</label>
-                <input type="phone" id="phoneNumber" />
-              </FormInput>
-              <FormInput>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
-              </FormInput>
+              {/* <InputWithLabel 
+                  label="Email"
+              /> */}
+              
             </Form>
-            <button>
-              <span>Continue</span>
-              <EastIcon />
-            </button>
+
           </DecisionContainer>
         </FormContainer>
       </SignUpWrapper>
--- src/components/InputWithLabel/InputWithLabel.tsx
+++ src/components/InputWithLabel/InputWithLabel.tsx
@@ -1,3 +1,10 @@
+import { FormInput } from "../../globals/styles/forms.styles";
+import {AiFillEye} from 'react-icons/ai';
+import {BsEyeSlashFill} from 'react-icons/bs'
+import { useState } from 'react';
+
+
+
 type Prop ={
     label:string;
     placeholder?:string;
@@ -5,15 +12,49 @@ type Prop ={
     register?:any;
     errorMessage?:string;
     type?:'password'|'text';
+    style?: React.CSSProperties;
+    // containerStyle:
   }
 
 
 
-const InputWithLabel = ({ type='text',errorMessage,label,placeholder='',isTextArea=false,register}:Prop):React.ReactElement=>{
+const InputWithLabel = ({style={}, type='text',errorMessage,label,placeholder='',isTextArea=false,register}:Prop):React.ReactElement=>{
+  const [show,setShow] = useState(false);
+  
+  const decideType =():string=>{
+    if(type=='password'){
+      return show?'text':'password'
+    }
+    return 'text'
+  }
+  const RenderIcon = ()=>{
+    return (
+      <>
+        {
+          show?
+            <BsEyeSlashFill onClick={(e)=> setShow(false)}/>
+            :
+            <AiFillEye onClick={(e)=> setShow(true)}/>
+        }
+      </>
+    )
+  }
+    return(
+      <FormInput style={style}>
+        <label htmlFor={label}>{label}</label>
+      {type==='password'&&<RenderIcon/>}
 
-    return <div>
+        {
+          !isTextArea?
+          <input  type={decideType()} id={label}  placeholder={placeholder} {...register} />
+        :
+        <textarea placeholder={placeholder}  id={label}>
 
-    </div>
+        </textarea>
+        }
+        <span style={{'color':'crimson'}}>{errorMessage}</span>
+      </FormInput>
+    )
 }
 
 
