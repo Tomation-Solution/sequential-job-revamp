49a1757e2b8440dc5379b859c624c6a890803800
markothedev <markothedevmail@gmail.com>
2023-06-16 11:11:31 +0100

    added env to the base url to avoid confusion

--- src/redux/api/axios.ts
+++ src/redux/api/axios.ts
@@ -3,8 +3,7 @@ import axios from 'axios'
 
 
 
-// export const url = 'https://recruitment-api-production.up.railway.app'
-export const url = 'http://localhost:8000'
+export const url =`${process.env.REACT_APP_BASE_URL}` 
 const api =axios.create({
   baseURL: url,
 });
