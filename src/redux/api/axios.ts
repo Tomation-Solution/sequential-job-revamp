a23d36e5f8babe406721ea2e2aceec9310cfab20
Samuel Ayomide <samuelayo61@gmail.com>
2024-09-06 14:43:45 +0100

    Added

--- src/redux/api/axios.ts
+++ src/redux/api/axios.ts
@@ -4,8 +4,8 @@ import axios from 'axios'
 
 
 // export const url =`${process.env.REACT_APP_BASE_URL}` 
-export const url =`https://sequentialjobs.watchdoglogisticsng.com`
-// export const url='http://localhost:8000'
+// export const url =`https://sequentialjobs.watchdoglogisticsng.com`
+export const url='http://localhost:8000'
 const api =axios.create({
   baseURL: url,
   // withCredentials:true
